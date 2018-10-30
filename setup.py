caf1e9ac56f35aa314deab96f2435ab31cd079a5
nerd-1 <ooyendyk@tuta.io>
2018-10-30 11:19:48 +0930

    Added setup.py and updated directory structure

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
+setuptools.setup(
+    name='visual_timer',  
+    version='0.1',
+    scripts=['visual_timer'] ,
+    author="nerd1",
+    authoremail="nerd1pypi@cock.li"
+    description="Visual timer is a simple terminal based timer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nerd-1/visual_timer",
+    keywords=["timer","ui"]
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 0.1 - Beta",
+        "License :: DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE",
+        "Operating System :: OS Independent",
+        "Topic :: Other/Nonlisted Topic",
+        "Enviroment :: Curses"
+    ],
+)
