6a76274a6628a675fa6ab68dc4f5045f084bb24e
nerd-1 <ooyendyk@tuta.io>
2018-11-14 14:34:08 +0930

    added unit test structure

---
+++ visual_timer/test_run.py
@@ -0,0 +1,31 @@
+
+import pytest
+
+from . import visual_timer
+
+class Test_check_input(object):
+	def test_check_input_returns_true_with_correct_format(self):
+		# TODO: Call 'check_input' and make sure it returns 'true' with the time format 'HH:MM:SS' 
+		pass
+	def test_check_input_returns_false_with_empty_string(self):
+		# TODO: Call 'check_input' and make sure it returns 'false' with an empty string 
+		pass
+	def test_check_input_returns_false_with_wrong_string(self):
+		# TODO: Call 'check_input' and make sure it returns 'false' with the string 'foobar' 
+		pass
+
+class Test_divide_y(object):
+	def test_divide_y_returns_the_correct_result():
+		# TODO: Call 'divide_y' and make sure it returns '0.5' where 'timer_set' is 120 and 'y_dim' is 60
+		pass
+
+class Test_render_y(object):
+	# TODO: Create test functions
+
+class Test_format_seconds(object):
+	# TODO: Create test functions
+
+class Test_convert_to_sec(object):
+	# TODO: Create test functions
+
+		
