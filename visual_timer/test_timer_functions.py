6d24e1b2155f6cb8f26644363020e61ad3ca4519
nerd-1 <ooyendyk@tuta.io>
2018-11-23 01:07:18 +0930

    Added more unit tests

--- visual_timer/test_timer_functions.py
+++ visual_timer/test_timer_functions.py
@@ -1,4 +1,11 @@
 
+import math
+import time
+import sys
+import os
+import re
+import curses
+
 import pytest
 
 import timer_functions
@@ -6,25 +13,79 @@ import timer_functions
 class Test_check_input(object):
 	def test_check_input_returns_true_with_correct_format(self):
 		# TODO: Call 'check_input' and make sure it returns 'true' with the time format 'HH:MM:SS' 
-		pass
+		result = timer_functions.check_input("00:00:05")
+		assert result == True
 	def test_check_input_returns_false_with_empty_string(self):
 		# TODO: Call 'check_input' and make sure it returns 'false' with an empty string 
-		pass
+		result = timer_functions.check_input("")
+		assert result == False
 	def test_check_input_returns_false_with_wrong_string(self):
 		# TODO: Call 'check_input' and make sure it returns 'false' with the string 'foobar' 
-		pass
+		result = timer_functions.check_input("foobar")
+		assert result == False
 
 def test_divide_y_returns_the_correct_result():
 	# TODO: Call 'divide_y' and make sure it returns '0.5' where 'timer_set' is 120 and 'y_dim' is 60
 	result = timer_functions.divide_y(120, 60)
 	assert result == 0.5
 
+'''
+# Giving up on this as it's too much work for very little benifit
+# https://stackoverflow.com/questions/90002/what-is-a-reasonable-code-coverage-for-unit-tests-and-why?rq=1
 class Test_render_y(object):
-	# TODO: Create test functions
-	pass
+	def test_render_y_returns_empty_screen(self):
+		
+		#define variables here
+		timer_set = 60				#What the timer was originally set to
+		seconds_remaining = None	#Used exclusivly as a local variable in the "format_seconds" function. Useless
+		formated_time = None
+		y_dim = 64					#height of screen
+		x_dim = 64					#width of screen
+
+		with timer_functions.curses_stdscr() as stdscr:
+			stdscr = curses.initscr()
+			stdscr.nodelay(1)
+			y_sec_unit = timer_functions.divide_y(timer_set, dimensions[0])
+			stdscr = timer_functions.render_y(seconds, y_sec_unit, y_pixel_count, stdscr, x_dim, y_dim)
+
+			pass
+
+
+	def test_render_y_returns_full_screen(self):
+		pass
+	def test_render_y_returns_screen_with_top_three_rows_white(self):
+		pass
+'''
 
 class Test_format_seconds(object):
 	# TODO: Create test functions
+	def test_format_seconds_seconds(self):
+		seconds = 0
+		timer_set = 2
+		result = timer_functions.format_seconds(seconds, timer_set)
+		assert result == "2"
+	def test_format_seconds_minutes(self):
+		seconds = 0
+		timer_set = 122
+		result = timer_functions.format_seconds(seconds, timer_set)
+		assert result == "2m 2s"
+	def test_format_seconds_hours(self):
+		seconds = 0
+		timer_set = 7322
+		result = timer_functions.format_seconds(seconds, timer_set)
+		assert result == "2h 2m 2s"
+	def test_format_seconds_days(self):
+		seconds = 0
+		timer_set = 180122
+		result = timer_functions.format_seconds(seconds, timer_set)
+		assert result == "2d 2h 2m 2s"
+	def test_format_seconds_years(self):
+		seconds = 0
+		timer_set = 63295322
+		result = timer_functions.format_seconds(seconds, timer_set)
+		assert result == "2y 2d 2h 2m 2s"
+	def test_format_seconds_zero(self):
+		pass
 	pass
 
 class Test_convert_to_sec(object):
--- visual_timer/timer_functions.py
+++ visual_timer/timer_functions.py
@@ -9,31 +9,35 @@ import curses
 seconds = 0
 
 def is_even(number):
+	#Returns a ham sandwich
 	if (number%2 == 0):
 		return('even')
 	return('odd')
 
 class curses_stdscr:
-    def __enter__(self):
-        self.stdscr = curses.initscr()
-        curses.cbreak()
-        curses.noecho()
-        self.stdscr.keypad(1)
-        stdscr_HEIGHT, stdscr_WIDTH = self.stdscr.getmaxyx()
-        return self.stdscr
-    def __exit__(self,a,b,c):
-        curses.nocbreak()
-        self.stdscr.keypad(0)
-        curses.echo()
-        curses.endwin()
+	#This is the screen object
+	def __enter__(self):
+		self.stdscr = curses.initscr()
+		curses.cbreak()
+		curses.noecho()
+		self.stdscr.keypad(1)
+		stdscr_HEIGHT, stdscr_WIDTH = self.stdscr.getmaxyx()
+		return self.stdscr
+	def __exit__(self,a,b,c):
+		curses.nocbreak()
+		self.stdscr.keypad(0)
+		curses.echo()
+		curses.endwin()
 
 def check_input(time_str):
+	#Takes the input "time_str" and returns "True" if it is a valid input
 	if re.match("[0-9][0-9]:[0-9][0-9]:[0-9][0-9]", time_str) != None:
 		return(True)
 	else:
 		return(False)
 
 def output_help():
+	#Outputs help to log
 	os.chdir("..")
 	os.chdir("docs")
 	file = open("usage.txt", "r")
@@ -42,11 +46,12 @@ def output_help():
 	file.close
 
 def divide_y(timer_set, y_dim):
-	#Divides the hight of screen by the set time (in secconds)
+	#Divides the hight of screen "y_dim" by the set time "timer_set" (in seconds) so we know how many seconds to wait before shading a new row
 	y_sec_unit = y_dim/timer_set
 	return(y_sec_unit)
 
 def render_y(seconds, y_sec_unit, y_pixel_count, stdscr, x_dim, y_dim):
+	#Renders the spacial representation of how much time is remaining to "stdscr"
 	cursor_y = 0
 	cursor_x = 0
 	for sec_unit in range(int(y_pixel_count)):
@@ -61,30 +66,35 @@ def render_y(seconds, y_sec_unit, y_pixel_count, stdscr, x_dim, y_dim):
 	return(stdscr)
 
 def render_time(seconds, stdscr, y_dim, x_dim, formated_time, timer_set):
+	#Renders the time from "format_seconds" on to "stdscr"
 	formated_time = format_seconds(seconds, timer_set)
 	stdscr.addstr(math.ceil(((y_dim)-1)/2), math.ceil((x_dim-len(formated_time))/2), formated_time, curses.A_BOLD)
 	return(stdscr, formated_time)
 
 def format_seconds(seconds, timer_set, hide_seconds=False):
-    #This function was taken from the termdown project and was written by trehn
-    seconds_remaining = timer_set - seconds
-    if seconds_remaining <= 60:
-        return str(seconds_remaining)
-    output = ""
-    for period, period_seconds in (
-        ('y', 31557600),
-        ('d', 86400),
-        ('h', 3600),
-        ('m', 60),
-        ('s', 1),
-    ):
-        if seconds_remaining >= period_seconds and not (hide_seconds and period == 's'):
-            output += str(int(seconds_remaining / period_seconds))
-            output += period
-            output += " "
-            seconds_remaining = seconds_remaining % period_seconds
-    return output.strip()
+	#Converts time from seconds into a human readable format
+	#This function was taken from the termdown project and was written by trehn
+	seconds_remaining = timer_set - seconds
+	if seconds_remaining <= 60:
+		return str(seconds_remaining)
+	output = ""
+	for period, period_seconds in (
+		('y', 31557600),
+		('d', 86400),
+		('h', 3600),
+		('m', 60),
+		('s', 1),
+	):
+		if seconds_remaining >= period_seconds and not (hide_seconds and period == 's'):
+			output += str(int(seconds_remaining / period_seconds))
+			output += period
+			output += " "
+			seconds_remaining = seconds_remaining % period_seconds
+	return output.strip()
 
 def convert_to_sec(time_str):
-    h, m, s = time_str.split(':')
-    return int(h) * 3600 + int(m) * 60 + int(s)
+	#Converts the input in the form "HH:MM:SS" to seconds
+	h, m, s = time_str.split(':')
+	return int(h) * 3600 + int(m) * 60 + int(s)
+
+
